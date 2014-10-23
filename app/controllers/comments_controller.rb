1751759794aec2c83ceb72e71427e3bf52ce7be6
kristian@purestyle.se <kristian@purestyle.se>
2014-10-23 14:32:20 +0200

    bye spambots hopefully

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
-  
+
   before_filter :authenticate, :only => :destroy
-  
+
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment])
+    #@comment = @post.comments.create(params[:comment])
     redirect_to post_path(@post)
   end
-  
+
   def destroy
     @post    = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
